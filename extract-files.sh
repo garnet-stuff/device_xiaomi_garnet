9abc85ac75e5b1db00cebd2463ef8fb8bdab6803
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-20 06:33:36 +0000

    garnet: Rename prebuilt libgrpc++_unsecure.so for QCC
    Change-Id: I7a0157e50c0eb0d1d84b4584f55e374f4b099483
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -100,9 +100,15 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/bin/qcc-trd)
+            "${PATCHELF}" --replace-needed "libgrpc++_unsecure.so" "libgrpc++_unsecure_prebuilt.so" "${2}"
+            ;;
         vendor/lib64/hw/audio.primary.parrot.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        vendor/lib64/libgrpc++_unsecure_prebuilt.so)
+            "${PATCHELF}" --set-soname "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
             [ "$2" = "" ] && return 0
